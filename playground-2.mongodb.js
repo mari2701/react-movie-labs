457561e655b0b6c55f2b6fd85c518e5292ef1522
Maria <20106500@mail.wit.ie>
2026-03-04 20:13:27 +0000

    Using react-query to manage the browser-based cache.

--- playground-2.mongodb.js
+++ playground-2.mongodb.js
@@ -1,1 +1,36 @@
-//NEW PLAYGROUND
+use ("rescue")
+
+
+//Exercises Update()
+//1
+db.animals.updateOne(
+    {name: "Catty", breed: "British Shorthair"},
+    {$set: {adopted: false}}
+)
+
+//2
+db.animals.updateOne(
+    {name: "Spot", breed: "Beagle"},
+    {$set: {"vaccinations.1": {type: "kennel cough", frequency: "yearly", amountMg: 2}}}
+)   
+
+//3
+
+
+//4
+db.animals.updateOne(
+    {_id: "MC-3012"},
+    {$push: {tags: "very very cute"}}
+)
+
+//5
+db.animals.updateOne(
+    {_id: "MC-1033"},
+    {$pop: {tags: 1}}
+)
+
+//6
+
+//7
+
+//8
