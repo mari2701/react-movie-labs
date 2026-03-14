672ed342744885bd2268b581b54f51db9e411462
Maria <20106500@mail.wit.ie>
2026-03-14 22:48:35 +0000

    New button for upcoming movies page.

---
+++ movies/src/components/cardIcons/addToWatchList.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PlaylistAddIcon from '@mui/icons-material/PlaylistAdd';
+
+const AddToWatchListIcon = ({ movie }) => {
+  return (
+    <PlaylistAddIcon color="primary" />
+  );
+};
+
+export default AddToWatchListIcon;
--- movies/src/pages/upcomingMoviesPage.jsx
+++ movies/src/pages/upcomingMoviesPage.jsx
@@ -4,6 +4,7 @@ import PageTemplate from '../components/templateMovieListPage';
 import { useQuery } from '@tanstack/react-query';
 import Spinner from '../components/spinner';
 import AddToFavoritesIcon from '../components/cardIcons/addToFavorites';
+import AddToWatchListIcon from '../components/cardIcons/addToWatchList';
 
 const UpcomingMoviesPage = () => {
   const { data, error, isPending, isError } = useQuery({
@@ -31,7 +32,12 @@ const UpcomingMoviesPage = () => {
       title="Upcoming Movies"
       movies={movies}
       action={(movie) => {
-        return <AddToFavoritesIcon movie={movie} />;
+        return (
+          <>
+            <AddToFavoritesIcon movie={movie} />
+            <AddToWatchListIcon movie={movie} />
+          </>
+        );
       }}
     />
   );
